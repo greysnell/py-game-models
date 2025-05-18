5461fd8a738dd74a606089542e104289be55ebe6
Greysnell Malave <greysnell20@gmail.com>
2025-05-18 16:43:20 -0300

    'solution'

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import init_django_orm  # noqa: F401
-
-from db.models import Race, Skill, Player, Guild
+from django.db import models
 
 
-def main() -> None:
-    pass
+class Race(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-if __name__ == "__main__":
-    main()
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+
+    def __str__(self):
+        return self.name
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='players')
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True, related_name='players')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
