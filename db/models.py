f1db62f36d449a78c11c5751e332baec77aa4a60
greysnell <greysnell20@gmail.com>
2025-05-18 17:35:27 -0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -20,7 +20,7 @@ class Skill(models.Model):
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(null=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
